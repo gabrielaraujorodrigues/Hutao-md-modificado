67a7b0e2b646b5e87f516e698b22e5471ce27fb9
Gabriel Rodrigues <129956127+gabrielaraujorodrigues@users.noreply.github.com>
2026-06-09 10:19:16 -0300

    add: config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    prefix: '!',
+    ownerNumber: '5500000000000',
+    botName: 'HutaoBot-MD',
+    ownerName: 'Dono',
+    footer: '🌸 HutaoBot-MD',
+    cooldown: 3000,
+    maxFileSize: 60 * 1024 * 1024,
+}
