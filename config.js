860d22ba593626dc12f6d60c56f3c2b7f326b997
Gabriel Rodrigues <129956127+gabrielaraujorodrigues@users.noreply.github.com>
2026-06-09 22:52:31 -0300

    feat: renomeia bot para Jordan Bot e adiciona URL da imagem do menu

--- config.js
+++ config.js
@@ -1,9 +1,11 @@
 module.exports = {
     prefix: '!',
     ownerNumber: '5500000000000',
-    botName: 'HutaoBot-MD',
+    botName: 'Jordan Bot',
     ownerName: 'Dono',
-    footer: '🌸 HutaoBot-MD',
+    footer: '🌸 Jordan Bot',
     cooldown: 3000,
     maxFileSize: 60 * 1024 * 1024,
+    // URL da imagem do menu (hospedada no próprio repositório)
+    menuImage: 'https://raw.githubusercontent.com/gabrielaraujorodrigues/Hutao-md-modificado/main/src/assets/menu.jpg',
 }
